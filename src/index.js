5c4b764fd6de6e50ea2ce58303aec3f8a7c5df05
nomen1 <olesiamakarenko5115@gmail.com>
2020-05-14 19:30:08 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import store from "./redux/store";
 import {Provider} from "react-redux"
 import 'semantic-ui-css/semantic.min.css'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
   <React.StrictMode>
      <Provider store={store}>
+     <BrowserRouter basename={process.env.PUBLIC_URL}>
+     </BrowserRouter>
     <App />
     </Provider>
   </React.StrictMode>,
@@ -18,3 +21,4 @@ ReactDOM.render(
 
 
 serviceWorker.unregister();
+
