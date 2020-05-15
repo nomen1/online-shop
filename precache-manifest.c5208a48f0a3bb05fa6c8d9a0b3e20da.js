651643748a36647f43ffeb29fe8f9211df3d7fc0
nomen1 <olesiamakarenko5115@gmail.com>
2020-05-15 10:26:23 +0300

    Updates

---
+++ precache-manifest.c5208a48f0a3bb05fa6c8d9a0b3e20da.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a21d8c67c6e74eb272155a983345bb9",
+    "url": "/nomen1/online-shop/index.html"
+  },
+  {
+    "revision": "22f2f452831510f40244",
+    "url": "/nomen1/online-shop/static/css/2.d18e6be8.chunk.css"
+  },
+  {
+    "revision": "0ea4751a9388d94ae435",
+    "url": "/nomen1/online-shop/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "22f2f452831510f40244",
+    "url": "/nomen1/online-shop/static/js/2.92d804ad.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/nomen1/online-shop/static/js/2.92d804ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea4751a9388d94ae435",
+    "url": "/nomen1/online-shop/static/js/main.7b40bd1b.chunk.js"
+  },
+  {
+    "revision": "751baed0cba236f5c18f",
+    "url": "/nomen1/online-shop/static/js/runtime-main.3c8fda30.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/nomen1/online-shop/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/nomen1/online-shop/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/nomen1/online-shop/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/nomen1/online-shop/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/nomen1/online-shop/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/nomen1/online-shop/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/nomen1/online-shop/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/nomen1/online-shop/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/nomen1/online-shop/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/nomen1/online-shop/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/nomen1/online-shop/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/nomen1/online-shop/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/nomen1/online-shop/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/nomen1/online-shop/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/nomen1/online-shop/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/nomen1/online-shop/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.663d4f0ab5814741a705c4c07f94998e.js"
+  "/nomen1/online-shop/precache-manifest.c5208a48f0a3bb05fa6c8d9a0b3e20da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nomen1/online-shop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
